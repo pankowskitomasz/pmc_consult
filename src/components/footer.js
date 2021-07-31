7fca37f24561e39e11abcd37b53daeab9e4b8d92
pankowskitomasz <pankowski_tomasz@o2.pl>
2021-07-31 14:41:25 +0200

    frontend: contact,register,login

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,12 +8,12 @@ class Footer extends Component{
     render(){
         return(      
             <footer>
-                <Container fluid className="d-flex align-items-center p-3 fixed-bottom z-index-0">
+                <Container fluid className="d-flex align-items-center p-3 border-top border-violet z-index-0">
                     <Row className="mx-auto text-center d-flex w-100">
                         <Col xs={10} className="mx-auto">
-                            <small className="my-0 text-dark px-3 py-1">
+                            <small className="my-0 text-violet px-3 py-1">
                                 Copyright &copy; 2021 Tomasz Pankowski. All rights reserved. 
-                                <Link to={this.props.privacyLink.href} className="text-dark text-decoration-none">
+                                <Link to={this.props.privacyLink.href} className="text-violet text-decoration-none">
                                     {this.props.privacyLink.name}
                                 </Link>
                             </small>
