5fb21aa6c61889f3d63ec3d6a758b5883da87418
TP <pankowski.tomasz@prokonto.pl>
2022-11-09 16:38:32 +0100

    fonts,gpdr,og tags

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,11 +8,11 @@ class Footer extends Component{
     render(){
         return(      
             <footer>
-                <Container fluid className="d-flex align-items-center p-3 border-top border-violet z-index-0">
+                <Container fluid className="d-flex align-items-center p-2 border-top border-violet z-index-0">
                     <Row className="mx-auto text-center d-flex w-100">
                         <Col xs={10} className="mx-auto">
                             <small className="my-0 text-violet px-3 py-1">
-                                Copyright &copy; 2021 Tomasz Pankowski. All rights reserved. 
+                                Copyright &copy; 2021-2022 Tomasz Pankowski. All rights reserved. 
                                 <Link to={this.props.privacyLink.href} className="text-violet text-decoration-none">
                                     {this.props.privacyLink.name}
                                 </Link>
